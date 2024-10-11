c0042a18a86209ffdc29423ba01c3f7366f91306
pskleer <p.s.kleer@tilburguniversity.edu>
2024-10-11 09:55:28 +0200

    Sample exam (correct)

---
+++ prog-tutorials/tutorial-week7-practice_exam/sample-block-3-template.py
@@ -0,0 +1,33 @@
+################################
+# 30M015 Sample Exam, Block 3. #
+################################
+
+# Grading (do not remove):
+# a) 
+# b) 
+# c) 
+# d) 
+# e) 
+
+snr = 1234567  # replace with your student number
+
+import pandas as pd
+df = pd.read_csv("sample-exam-data.csv")
+
+### Question a) ###
+# Total number of days in the dataset the company had an advertising campaign:
+
+### Question b) ###
+# Create a new column/variable in df called daily_revenue:
+
+### Question c) ###
+# The total revenue the company made in June 2023:
+
+### Question d) ###
+# The average number of units sold per day on days where either there was an
+# advertising campaign or there was a holiday:
+
+### Question e) ###
+# The average number of units sold per day during the advertising campaign
+# minus the average number of units sold per day when there was no advertising
+# campaign.
